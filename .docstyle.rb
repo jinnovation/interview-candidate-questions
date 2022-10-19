b8563e88b8468835b78cfd17f52de55b55a93e66
Jonathan Jin <me@jonathanj.in>
2022-10-18 22:41:28 -0400

    add markdown linting

---
+++ .docstyle.rb
@@ -0,0 +1,24 @@
+# Markdown style configuration.
+#
+# For details on how to configure, see [markdownlint docs][1].
+#
+# For explanation of the rules themselves, see [markdownlint's RULES.md][2]
+#
+# [1]: https://github.com/markdownlint/markdownlint/blob/master/docs/creating_styles.md
+# [2]: https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+
+tag :whitespace
+tag :headers
+tag :ul
+tag :indentation
+tag :bullet
+
+# Techdocs transitively requires 4-space indentation, since Python-Markdown strictly abides by the Markdown spec
+rule "ul-indent", indent: 4
+
+rule "ul-style", style: :dash
+rule "no-duplicate-header", allow_different_nesting: true
+rule "line-length", :line_length => 120
+
+exclude_rule 'MD026'            # Trailing punctuation in header
+
